650105bd3df2b56e0509c2aa279917d04fdffa54
Julian Gernun <17549662+jcger@users.noreply.github.com>
2023-05-31 17:26:42 +0200

    remove everything dispensable

--- README.md
+++ README.md
@@ -4,139 +4,10 @@ In our API, there are certain parameters that have not been documented. This lac
 
 ## How it works
 
-1. This tool searches for yaml files that contain `@kbn-doc-linker entry` (entry files) and `@kbn-doc-linker partial` (partial files) as header comment
-2. It searches for missing definitions in the yaml files marked as entry file
-3. It searches for those missing definitions in the yaml files marked as partial file
-4. It adds those missing definitions to the entry file and creates a new file
-
-Example `openapi/demo` folder:
-
-The entry file is called `example.yaml`, the first line indicates that it is an entry file:
-
-```yaml
-# @kbn-doc-linker entry
-
-openapi: 3.0.1
-info:
-  title: Your API
-  version: 1.0.0
-  license:
-    name: "MIT"
-    url: "fakeurl"
-servers:
-  - url: http://localhost:5601
-    description: local
-paths:
-  /example:
-    post:
-      operationId: createExample
-      summary: Create a new example
-      requestBody:
-        required: true
-        content:
-          application/json:
-            schema:
-              oneOf:
-                - $ref: "#/components/schemas/NewSchema"
-                - $ref: "#/components/schemas/AnotherSchema" # this definition is missing in this file
-      responses:
-        "200":
-          description: OK
-        "400":
-          description: KO
-components:
-  securitySchemes:
-    basicAuth:
-      type: http
-      scheme: basic
-  schemas:
-    NewSchema:
-      type: object
-      properties:
-        email:
-          type: string
-        age:
-          type: integer
-      required:
-        - email
-security:
-  - basicAuth: []
-```
-
-And we have an `another_schema.yaml` file that looks like this:
-
-```yaml
-# @kbn-doc-linker partial
-
-AnotherSchema: # this is the definition missing in the entry file
-  type: object
-  properties:
-    name:
-      type: string
-    address:
-      type: string
-  required:
-    - name
-```
-
-The first line indicates that it is a partial spec definition. When running this tool, the output will be
-
-```yaml
-openapi: 3.0.1
-info:
-  title: Your API
-  version: 1.0.0
-  license:
-    name: MIT
-    url: fakeurl
-servers:
-  - url: http://localhost:5601
-    description: local
-paths:
-  /example:
-    post:
-      operationId: createExample
-      summary: Create a new example
-      requestBody:
-        required: true
-        content:
-          application/json:
-            schema:
-              oneOf:
-                - $ref: "#/components/schemas/NewSchema"
-                - $ref: "#/components/schemas/AnotherSchema"
-      responses:
-        "200":
-          description: OK
-        "400":
-          description: KO
-components:
-  securitySchemes:
-    basicAuth:
-      type: http
-      scheme: basic
-  schemas:
-    NewSchema:
-      type: object
-      properties:
-        email:
-          type: string
-        age:
-          type: integer
-      required:
-        - email
-    AnotherSchema:
-      type: object
-      properties:
-        name:
-          type: string
-        address:
-          type: string
-      required:
-        - name
-security:
-  - basicAuth: []
-```
+1. It searches for all `entrypoint.yaml` files
+2. For each one of them, it will follow links to files until it finds a reference to something that is no link
+3. It will search for yaml files named after the previous reference
+4. It will replace the reference to a relative dir to the file found in the previous step
 
 ## Installation
 
@@ -145,8 +16,6 @@ Clone the repo and run `npm install`
 ## Usage
 
 Generate the linked documentation splitted in the `openapi` folder by running `npm run build`
-
-This will create a folder called `build` with the same file/folder tree as the `openapi` folder but will join the entry point yaml spec definitions with its missing parts.
 
 ## Licence
 
--- src/index.ts
+++ src/index.ts
@@ -1,109 +1,35 @@
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0 and the Server Side Public License, v 1; you may not use this file except
- * in compliance with, at your election, the Elastic License 2.0 or the Server
- * Side Public License, v 1.
- */
 import * as glob from "glob"
-import * as path from "path"
-import { loadYamlFile, saveYamlFile } from "./io"
+import redocly from "@redocly/openapi-core"
+import { loadYamlFile } from "./io"
+import path from "path"
 
 export interface OpenAPISpec {
   $ref?: string
   [key: string]: any
 }
 
-// Recursively find all $ref values in the spec and add them to a set
-function findRefs(
-  obj: OpenAPISpec,
-  refs: Set<string>,
-  fileRefs: Set<string>,
-): void {
-  for (let key in obj) {
-    if (typeof obj[key] === "object" && obj[key] !== null) {
-      findRefs(obj[key], refs, fileRefs)
-    } else if (key === "$ref") {
-      if (obj[key].startsWith("#")) {
-        refs.add(obj[key])
-      } else {
-        fileRefs.add(obj[key])
-      }
+function getValueFromPath(obj: OpenAPISpec, path: string) {
+  if (path.startsWith("#/")) {
+    path = path.substring(2)
+  }
+  // redocly uses ~1 for / in paths, for example application/json is valid
+  const paths = path.split("/").map((p: string) => p.replace("~1", "/"))
+  let current = obj
+
+  for (let i = 0; i < paths.length; i++) {
+    if (current[paths[i]] === undefined) {
+      return undefined
+    } else {
+      current = current[paths[i]]
     }
   }
+
+  return current
 }
 
-// Check whether a spec contains a specific $ref
-function checkRef(obj: OpenAPISpec, ref: string): boolean {
-  for (let key in obj) {
-    if (typeof obj[key] === "object" && obj[key] !== null) {
-      if (checkRef(obj[key], ref)) {
-        return true
-      }
-    } else if (key === "$ref" && obj[key] === ref) {
-      return true
-    }
-  }
-  return false
-}
-
-function processFiles(
-  entryFilePath: string,
-  files: string[],
-  basePath: string,
-): void {
-  const allRefs = new Set<string>()
-  const fileRefs = new Set<string>()
-  const visitedFiles = new Set<string>()
-
-  // First, find all $ref values in the entry file
-  let entryYamlData = loadYamlFile(entryFilePath)
-  findRefs(entryYamlData, allRefs, fileRefs)
-
-  while (fileRefs.size > 0) {
-    const fileRef = fileRefs.values().next().value
-    fileRefs.delete(fileRef)
-
-    if (visitedFiles.has(fileRef)) {
-      continue
-    }
-    visitedFiles.add(fileRef)
-
-    const filePath = path.join(basePath, fileRef)
-    console.log("loading file", filePath)
-    const yamlData = loadYamlFile(filePath)
-    findRefs(yamlData, allRefs, fileRefs)
-  }
-
-  // Then, check whether each ref is defined in any of the other files
-  for (const ref of allRefs) {
-    let found = false
-    for (const fileRef of visitedFiles) {
-      const filePath = path.join(basePath, fileRef)
-      const yamlData = loadYamlFile(filePath)
-      if (checkRef(yamlData, ref)) {
-        found = true
-        break
-      }
-    }
-    if (!found) {
-      for (const file of files) {
-        const filePath = path.join(basePath, file)
-        const yamlData = loadYamlFile(filePath)
-        if (checkRef(yamlData, ref)) {
-          const relativePath = path.relative(
-            path.dirname(entryFilePath),
-            filePath,
-          )
-          const newRef = `./${relativePath}#${ref.slice(1)}`
-          console.log("Replacing", ref, "with", newRef, "in", entryFilePath)
-          // replaceRef(entryYamlData, ref, newRef)
-          // saveYamlFile(entryFilePath, entryYamlData)
-          break
-        }
-      }
-    }
-  }
+const getDefinition = (location: redocly.LocationObject) => {
+  const spec = loadYamlFile(location.source.absoluteRef)
+  return getValueFromPath(spec, location.pointer)
 }
 
 function replaceRef(obj: OpenAPISpec, oldRef: string, newRef: string): void {
@@ -117,17 +43,62 @@ function replaceRef(obj: OpenAPISpec, oldRef: string, newRef: string): void {
 }
 
 const main = async ({ sourceDir = "./openapi" }: { sourceDir: string }) => {
-  const fileNames = glob.glob.sync(`${sourceDir}/**/*.yaml`, {
+  const files = glob.glob.sync(`${sourceDir}/**/*.yaml`, {
     root: sourceDir || process.cwd(),
   })
-  fileNames.forEach((file: string) => {
-    if (file.endsWith("entrypoint.yaml")) {
-      const fileDir = path.dirname(file)
-      processFiles(path.join(sourceDir, file), fileNames, fileDir)
-    }
+
+  files.forEach((entryPointFile: string) => {
+    if (!entryPointFile.endsWith("entrypoint.yaml")) return
+
+    redocly
+      .bundle({
+        ref: entryPointFile,
+        config: new redocly.Config({}),
+        dereference: false,
+      })
+      .then((bundle) => {
+        bundle.problems.forEach((problem) => {
+          if (problem.message !== "Can't resolve $ref") return
+
+          problem.location.forEach((location) => {
+            if (!location.pointer) return
+
+            const definitionPath = getDefinition(location)
+            if (!definitionPath || !definitionPath.$ref) return
+
+            const missingDefinition = definitionPath.$ref
+              .split("/")
+              .slice(-1)[0]
+
+            const found = files.some((file: string) => {
+              if (!file.endsWith(`${missingDefinition}.yaml`)) return false
+
+              const relativePath = path.relative(
+                path.dirname(location.source.absoluteRef),
+                file,
+              )
+
+              // TODO: Open "absoluteRef" file, go to the "pointer" position and replace the $ref to be "relativePath"
+              const spec = loadYamlFile(location.source.absoluteRef)
+              const definition = getValueFromPath(spec, location.pointer)
+              if (definition && definition.$ref) {
+                replaceRef(spec, definition.$ref, relativePath)
+
+                const yaml = jsYaml.safeDump(spec)
+                fs.writeFileSync(location.source.absoluteRef, yaml, "utf8")
+              }
+
+              return true
+            })
+
+            if (!found) {
+              console.error("Unknown error, run @readcly/cli for details")
+              process.exit(1)
+            }
+          })
+        })
+      })
   })
 }
 
-if (process.env.NODE_ENV !== "test") {
-  main({ sourceDir: "." })
-}
+main({ sourceDir: "./openapi" })
