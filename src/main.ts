60624effc71a80aed7c0a3cfb13650f8e9a6cd09
Julian Gernun <17549662+jcger@users.noreply.github.com>
2023-05-31 16:58:34 +0200

    find missing definitions

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import * as glob from "glob"
 import redocly from "@redocly/openapi-core"
 import { loadYamlFile } from "./io"
+import path from "path"
 
-function getValueFromPath(obj: { [key in string]: any }, path: string) {
+interface OpenAPISpec {
+  $ref?: string
+  [key: string]: any
+}
+
+function getValueFromPath(obj: OpenAPISpec, path: string) {
   if (path.startsWith("#/")) {
     path = path.substring(2)
   }
+  // redocly uses ~1 for / in paths, for example application/json is valid
   const paths = path.split("/").map((p: string) => p.replace("~1", "/"))
   let current = obj
 
@@ -27,32 +34,69 @@ const getDefinition = (location: redocly.LocationObject) => {
   return getValueFromPath(spec, location.pointer)
 }
 
+function replaceRef(obj: OpenAPISpec, oldRef: string, newRef: string): void {
+  for (let key in obj) {
+    if (typeof obj[key] === "object" && obj[key] !== null) {
+      replaceRef(obj[key], oldRef, newRef)
+    } else if (key === "$ref" && obj[key] === oldRef) {
+      obj[key] = newRef
+    }
+  }
+}
+
 const main = async ({ sourceDir = "./openapi" }: { sourceDir: string }) => {
-  const fileNames = glob.glob.sync(`${sourceDir}/**/*.yaml`, {
+  const files = glob.glob.sync(`${sourceDir}/**/*.yaml`, {
     root: sourceDir || process.cwd(),
   })
-  fileNames.forEach((file: string) => {
-    if (!file.endsWith("entrypoint.yaml")) return
+
+  files.forEach((entryPointFile: string) => {
+    if (!entryPointFile.endsWith("entrypoint.yaml")) return
 
     redocly
       .bundle({
-        ref: file,
+        ref: entryPointFile,
         config: new redocly.Config({}),
         dereference: false,
       })
       .then((bundle) => {
         bundle.problems.forEach((problem) => {
+          if (problem.message !== "Can't resolve $ref") return
+
           problem.location.forEach((location) => {
-            // TODO: check that the error type is missing $ref
-            const missingDefinition = getDefinition(location)
-              .$ref.split("/")
+            const definitionPath = getDefinition(location)
+            if (!definitionPath || !definitionPath.$ref) return
+
+            const missingDefinition = definitionPath.$ref
+              .split("/")
               .slice(-1)[0]
 
-            fileNames.forEach((file: string) => {
-              if (!file.endsWith(`${missingDefinition}.yaml`)) return
+            console.log("missingDefinition", missingDefinition)
+            // TODO: what should happen if there is more than one?
+            const found = files.some((file: string) => {
+              if (!file.endsWith(`${missingDefinition}.yaml`)) return false
 
-              console.log("found missing definition file", file)
+              const relativePath = path.relative(
+                path.dirname(entryPointFile),
+                file,
+              )
+              const newRef = `./${relativePath}/${file.slice(-1)}`
+              console.log(
+                "Replacing",
+                definitionPath,
+                "with",
+                newRef,
+                "in",
+                entryPointFile,
+              )
+              // replaceRef(entryYamlData, ref, newRef)
+              // saveYamlFile(entryFilePath, entryYamlData)
+              return true
             })
+
+            if (!found) {
+              console.error("Unknown error, run @readcly/cli for details")
+              process.exit(1)
+            }
           })
         })
       })
